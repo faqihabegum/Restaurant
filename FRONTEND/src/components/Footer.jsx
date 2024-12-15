a1003187cdb9efacafa33bc5a2ce733ff9a102ed
faqiha begum <faqihabegum10@gmail.com>
2024-12-15 16:10:47 +0530

    Adding files

---
+++ FRONTEND/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">FAQIHA BEGUM</div>
+          <div className="right">
+            <p>Hyderabad,India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Faqiha Begum</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Faqiha Begum.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
